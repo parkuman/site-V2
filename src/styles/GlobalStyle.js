91ff4d2097a8055b8277b03b965d37b6170a719d
Parkuman <parkumania@gmail.com>
2020-06-04 14:48:36 -0400

    configures 2 fonts

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,8 +1,10 @@
 import theme from "@styles/theme"
+import FontFaces from "@styles/fonts"
 import { createGlobalStyle } from "styled-components"
 
-
 const GlobalStyle = createGlobalStyle`
+${FontFaces};
+
 html {
     box-sizing: border-box;
     width: 100%;
@@ -17,15 +19,19 @@ body {
     margin: 0;
     width: 100%;
     min-height: 100%;
+    font-family: ${theme.fonts.IBMPlexSans};
     background: ${theme.colors.floralWhite};
     color: ${theme.colors.richBlack};
-    
     &.dark-mode {
         background: ${theme.colors.queenBlue};
         color: ${theme.colors.aquaMarine};
     } 
     transition: ${theme.transition}
 } 
+
+footer {
+    font-family: ${theme.fonts.IBMPlexMono};
+}
 `
 
 export default GlobalStyle
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -8,7 +8,12 @@ const theme = {
     candyPink: "#EF767A",
   },
 
-  transition: 'all 0.25s cubic-bezier(0.645, 0.045, 0.355, 1)',
+  fonts: {
+    IBMPlexMono: "IBM Plex Mono, monospace",
+    IBMPlexSans: "IBM Plex Sans, sans-serif",
+  },
+
+  transition: "all 0.25s cubic-bezier(0.645, 0.045, 0.355, 1)",
 }
 
 export default theme
