91da9600c4aa1dec0b2c96b3c93cac0c5ed24ef2
Parkuman <parkumania@gmail.com>
2020-06-23 17:15:12 -0400

    modify file case to try and fix netlify deploy

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { graphql } from "gatsby";
 
-import Layout from "@components/layout";
-import SEO from "@components/seo";
+import Layout from "@components/Layout";
+import SEO from "@components/SEO";
 
 import Hero from "@components/sections/hero";
 import About from "@components/sections/about";
